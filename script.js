644f4162ca337799034cf17334b55a80f9d1113f
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-06-16 21:03:40 +0200

    added light and dark mode that adjust to system theme

--- script.js
+++ script.js
@@ -4,6 +4,53 @@ let backgroundColor = style.getPropertyValue("--background-color");
 let gridColor = style.getPropertyValue("--grid-color");
 let boidColor = style.getPropertyValue("--boid-color");
 
+const themes = {
+  light: {
+    "--background-color": " #f2f2f2",
+    "--boid-color": " #e08aff",
+    "--grid-color": " #0d091c",
+    "--text-color": " #000",
+  },
+  dark: {
+    "--background-color": "#000000",
+    "--boid-color": "#e08aff",
+    "--grid-color": "#7264a4",
+    "--text-color": "#ffffff",
+  },
+};
+
+function switchTheme(themeName) {
+  const theme = themes[themeName];
+  if (!theme) {
+    console.log("no such theme as ", themeName);
+    return;
+  }
+  for (const variable in theme) {
+    if (theme.hasOwnProperty(variable)) {
+      document.documentElement.style.setProperty(variable, theme[variable]);
+    }
+  }
+}
+
+function updateThemeToSystem() {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    switchTheme("dark");
+  } else {
+    switchTheme("light");
+  }
+}
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    updateThemeToSystem();
+  });
+
+updateThemeToSystem();
+
 function update() {
   backgroundColor = style.getPropertyValue("--background-color");
   gridColor = style.getPropertyValue("--grid-color");
